105b19f5f0bb0a4e40ea8892e959132bb90b2611
maudhelen <mhovland.ieu2021@student.ie.edu>
2023-10-11 15:57:09 +0200

    uat deployment

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -40,3 +40,9 @@ with app.app_context():
 CORS(app)
 
 from iebank_api import routes
+if(os.getenv('ENV') == 'dev' or os.getenv('ENV') == 'uat'):
+    appinsights = AppInsights(app)
+    @app.after_request
+    def after_request(response):
+        appinsights.flush()
+        return response
